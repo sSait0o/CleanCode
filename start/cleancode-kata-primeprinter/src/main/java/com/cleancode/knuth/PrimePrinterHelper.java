df6c8cedf78006abacb77d61d68de5c69c181560
Quentin Leboucher <qleboucher1@gmail.com>
2024-12-13 08:40:24 +0100

    Activité 4 et 5

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
@@ -2,13 +2,12 @@ package com.cleancode.knuth;
 
 public class PrimePrinterHelper {
 
-    private final int numberOfPrimes = 1000;
-    private final int[] primes = new int[numberOfPrimes + 1];
     private final int linesPerPage = 50;
     private final int columns = 4;
     private final int ordmax = 30;
     private final int[] multiples = new int[ordmax + 1];
     private int pagenumber = 1;
+
     private int pageoffset = 1;
     private int rowoffset;
     private int column;
@@ -19,47 +18,51 @@ public class PrimePrinterHelper {
     private int square = 9;
     private int n = 0;
 
-    public void print() {
-        primes[1] = 2;
+    public int[] invoke(int numberOfNumbers) {
+        int[] numbers = new int[numberOfNumbers + 1];
+        numbers[1] = 2;
 
-        while (primeIndex < numberOfPrimes) {
+        while (primeIndex < numberOfNumbers) {
             do {
                 candidate += 2;
                 if (candidate == square) {
                     ord++;
-                    square = primes[ord] * primes[ord];
+                    square = numbers[ord] * numbers[ord];
                     multiples[ord - 1] = candidate;
                 }
                 n = 2;
                 possiblyPrime = true;
                 while (n < ord && possiblyPrime) {
                     while (multiples[n] < candidate)
-                        multiples[n] += primes[n] + primes[n];
+                        multiples[n] += numbers[n] + numbers[n];
                     if (multiples[n] == candidate)
                         possiblyPrime = false;
                     n++;
                 }
             } while (!possiblyPrime);
             primeIndex++;
-            primes[primeIndex] = candidate;
+            numbers[primeIndex] = candidate;
         }
 
-        while (pageoffset <= numberOfPrimes) {
+        return numbers;
+    }
+
+    public void printNumbers(int[] numbers, int numberOfNumbers) {
+        while (pageoffset <= numberOfNumbers) {
             System.out.print("The First ");
-            System.out.print(Integer.toString(numberOfPrimes));
+            System.out.print(Integer.toString(numberOfNumbers));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(pagenumber));
             System.out.println("\n");
             for (rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
                 for (column = 0; column <= columns - 1; column++)
-                    if (rowoffset + column * linesPerPage <= numberOfPrimes)
-                        System.out.printf("%10d", primes[rowoffset + column * linesPerPage]);
+                    if (rowoffset + column * linesPerPage <= numberOfNumbers)
+                        System.out.printf("%10d", numbers[rowoffset + column * linesPerPage]);
                 System.out.println();
             }
             System.out.println("\f");
             pagenumber++;
             pageoffset += linesPerPage * columns;
-
         }
     }
 }
